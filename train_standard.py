5fb86e4dab65d86d844ebad70c628efae40d78fc
dwDavidxd <85202634+dwDavidxd@users.noreply.github.com>
2023-10-11 10:29:12 +0800

    Update train_standard.py

--- train_standard.py
+++ train_standard.py
@@ -48,7 +48,7 @@ parser.add_argument('--seed', type=int, default=1, metavar='S',
                     help='random seed (default: 1)')
 parser.add_argument('--log-interval', type=int, default=200, metavar='N',
                     help='how many batches to wait before logging training status')
-parser.add_argument('--model-dir', default='./checkpoint/resnet_18/Standard_bs_128',
+parser.add_argument('--model-dir', default='./checkpoint/resnet_18/Standard_AT',
                     help='directory of model for saving checkpoint')
 parser.add_argument('--save-freq', '-s', default=90, type=int, metavar='N',
                     help='save frequency')
